e535c793a34aff24a96296da0a1ce348bf01be05
Jacob Mompean <jaymompean@gmail.com>
2022-12-03 15:22:17 -0600

    map todos to TodoList

--- app/todos/TodosList.tsx
+++ app/todos/TodosList.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
 import React from 'react';
 
 const fetchTodos = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
   const todos: Todo[] = await res.json();
+  return todos;
 };
 
 async function TodosList() {
   const todos = await fetchTodos();
 
-  return <div>TodosList</div>;
+  return (
+    <>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          <Link href={`/todos/${todo.id}`}>Todo: {todo.id}</Link>
+        </p>
+      ))}
+    </>
+  );
 }
 
 export default TodosList;
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import TodosList from './TodosList';
 
 function Todos() {
-  return <div>Todos</div>;
+  return (
+    <div>
+      <h1>Todos</h1>
+      <TodosList />
+    </div>
+  );
 }
 
 export default Todos;
