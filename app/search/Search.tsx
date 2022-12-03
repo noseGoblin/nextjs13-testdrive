c526550cc9c147a9444b59e4a3001685e5478ace
Jacob Mompean <jaymompean@gmail.com>
2022-12-03 16:36:38 -0600

    add Search component with search form

--- app/search/Search.tsx
+++ app/search/Search.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React, { FormEvent, useState } from 'react';
 
 function Search() {
-  return <div>I am a search component</div>;
+  const [search, setSearch] = useState();
+  const router = useRouter();
+
+  const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch('');
+    router.push(`/search/${search}`);
+  };
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type='text'
+        value={search}
+        placeholder='Enter the Search term'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button type='submit' className='btn'>
+        Search
+      </button>
+    </form>
+  );
 }
 
 export default Search;
