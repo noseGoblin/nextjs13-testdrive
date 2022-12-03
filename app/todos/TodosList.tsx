5d4bbe78766bdccac3471aed139730427360292c
Jacob Mompean <jaymompean@gmail.com>
2022-12-03 15:17:08 -0600

    add Todo typing and cast to todos

--- app/todos/TodosList.tsx
+++ app/todos/TodosList.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const fetchTodos = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
-  const todos = await res.json();
+  const todos: Todo[] = await res.json();
 };
 
 async function TodosList() {
