6a4d25b2c89ca47215f35a2e8fb3d8528a35ed19
Jacob Mompean <jaymompean@gmail.com>
2022-12-03 15:08:10 -0600

    add ToDos page & start back to home Header button

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
 
 function Header() {
   return (
     <header className='p-5 bg-blue-500'>
       <p className='font-bold text-white'>I am the Header</p>
+      <Link></Link>
     </header>
   );
 }
