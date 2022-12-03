35e5f756812af651430e79cb255719707e696dd4
Jacob Mompean <jaymompean@gmail.com>
2022-12-03 17:47:40 -0600

    add Suspense api & random Promise timers to show the separate loading streams independently on the homepage

--- app/(users)/todos/TodosList.tsx
+++ app/(users)/todos/TodosList.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 import { Todo } from '../../../typings';
 
 const fetchTodos = async () => {
+  // timeout for random number of seconds between 1 and 5
+  const timeout = Math.floor(Math.random() * 5 + 1) * 1000;
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+
   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
   const todos: Todo[] = await res.json();
-  // console.log('This is the todos', todos);
   return todos;
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,21 @@ import TodosList from './(users)/todos/TodosList';
 
 function Home() {
   return (
-    <div>
-      <Suspense fallback={<p>Loading the Todos...</p>}>
-        <h1>Loading Todos</h1>
+    <div className='flex flex-col space-y-10'>
+      <Suspense fallback={<p className='text-red-500'>Loading the Todos...</p>}>
         <div className='flex space-x-2'>
           {/* @ts-ignore */}
           <TodosList />
         </div>
       </Suspense>
 
-      <Suspense fallback={<p>Loading the Shopping Trolley...</p>}>
-        <h1>Loading Shopping Trolley</h1>
+      <Suspense
+        fallback={
+          <p className='text-blue-500'>Loading the Shopping Trolley...</p>
+        }
+      >
         <div className='flex space-x-2'>
+          {/* @ts-ignore */}
           <TodosList />
         </div>
       </Suspense>
